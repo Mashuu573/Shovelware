36eed7c5b35b14fccdb42efa77e5de27f75ef122
Torres Mauricio Ezequiel <maurex05@gmail.com>
2026-06-01 01:52:03 -0300

    Actualizacion proyecto

--- Toolbar.py
+++ Toolbar.py
@@ -5,39 +5,62 @@ TBFLAGS = ( wx.TB_HORIZONTAL
             | wx.TB_FLAT
             )
 
-
-class TestToolBar(wx.Frame):
+class MySplitter(wx.SplitterWindow):
     def __init__(self, parent):
-        super().__init__(parent, title="tittle", size=(600, 400))
         
+        super().__init__(parent, style=wx.SP_LIVE_UPDATE | wx.SP_3D)
+
+
+
+class MainFrame(wx.Frame):
+    def __init__(self):
+        super().__init__(parent=None, title="Shovelware", size=(600, 400))
         self.Bind(wx.EVT_CLOSE, self.OnCloseWindow)
+        splitter = MySplitter(self)
 
-        client = wx.Panel(self)
-        client.SetBackgroundColour(wx.WHITE)
+        sty = wx.BORDER_SUNKEN
 
         
+        p1 = wx.Window(splitter, style=sty)
+        
+        contacto_lista = wx.StaticText(p1, -1, "Lista de Contactos", (10, 10))
 
-        tb = wx.ToolBar(client, style=TBFLAGS)
+        
+        p2 = wx.Window(splitter, style=sty)
+        
+        contacto_actual = wx.StaticText(p2, -1, "Contacto actual", (10, 10))
+
+        splitter.SetMinimumPaneSize(50)  
+        
+        
+        splitter.SplitVertically(p1, p2, -100)
+
+        self.CreateToolBar(TBFLAGS)
+
         sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(tb, 0, wx.EXPAND)
+        sizer2 = wx.BoxSizer(wx.VERTICAL)
 
-        t1 = wx.TextCtrl(client, -1, "Mensaje", size=(125, -1))
-        sizer.Add(t1, 0, wx.EXPAND)
-        
-        client.SetSizer(sizer)
+        p1.SetSizer(sizer2)
+        sizer2.Add(contacto_lista, 0, wx.ALIGN_CENTER)
 
-        
-        
+        p2.SetSizer(sizer)
 
-        
+        sizer.Add(contacto_actual, 0, wx.ALIGN_CENTER)
+
+        mensajes_recibidos = wx.TextCtrl(p2, 1, "Mensaje recibido", size=(125, -1))
+        mensaje_enviar =  wx.TextCtrl(p2, 0, "Mensaje", size=(125, -1))
+        sizer.Add(mensajes_recibidos, 1, wx.EXPAND)
+        sizer.Add(mensaje_enviar, 0, wx.EXPAND)
+    
     def OnCloseWindow(self, event):
         self.Destroy()       
 
 
 
+
 if __name__ == '__main__':
     app = wx.App()
-    panel = TestToolBar(None)
+    panel = MainFrame()
     panel.Center()
     panel.Show()
     app.MainLoop()
